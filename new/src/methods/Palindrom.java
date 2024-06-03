75d46aba0f26cac76720ad46fa25afd5f96e3ad0
666Deadsoul <sarinamaharjan234@gmail.com>
2024-06-03 16:41:18 +0545

    palindrom

---
+++ new/src/methods/Palindrom.java
@@ -0,0 +1,23 @@
+package methods;
+
+public class Palindrom {
+    public void checker(int num){
+        int rev=0;
+        int number=num;
+       while(number!=0){
+        int div=number%10;
+        number=number/10;
+        rev= (rev*10)+div;
+       }
+        if(num == rev){
+            System.out.println(num+" is Palindrom");
+        }
+        else{
+            System.out.println(num +" is not Palindrom");
+        }
+    }
+    public static void main(String[] args) {
+        Palindrom p = new Palindrom();
+        p.checker(102201);
+    }
+}
