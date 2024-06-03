83627ebaa3acf7bf416d8f77c444db53aaf7901e
666Deadsoul <sarinamaharjan234@gmail.com>
2024-06-03 16:27:53 +0545

    oddeven

---
+++ new/src/methods/OddEven.java
@@ -0,0 +1,23 @@
+package methods;
+import java.util.Scanner;
+public class OddEven {
+    public boolean Checker(int n){
+        if(n%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        OddEven oe = new OddEven();
+        Scanner sc= new Scanner(System.in);
+        int num = sc.nextInt();
+        boolean status = oe.Checker(num);
+        if(status){
+            System.out.println("It is Even");
+        }else{
+            System.out.println("It is Odd");
+        }
+    }  
+}
