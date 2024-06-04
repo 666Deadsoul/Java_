24fae4252834b2897e1c25dbb28f5a6ddf7c96a9
666Deadsoul <sarinamaharjan234@gmail.com>
2024-06-04 17:09:49 +0545

    string array

---
+++ new/src/method/Str_arr.java
@@ -0,0 +1,10 @@
+package method;
+
+public class Str_arr {
+    public static void main(String[] args) {
+        String [] name={"Jane", "Anne", "Kim", "Luciana", "Emma"};
+        for(String s :name){
+            System.out.println(s);
+        }
+    }
+}
