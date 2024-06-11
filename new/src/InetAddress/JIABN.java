a376354da6f561f1934e3d94b3207fe395184f55
666Deadsoul <sarinamaharjan234@gmail.com>
2024-06-11 21:48:14 +0545

    Inet in java

---
+++ new/src/InetAddress/JIABN.java
@@ -0,0 +1,15 @@
+package InetAddress;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class JIABN {
+    public static void main(String[] args) {
+        try{
+            InetAddress ad = InetAddress.getByName("www.javapoint.com");
+            System.out.println(ad);
+        }
+        catch(UnknownHostException ex){
+            System.out.println(ex);
+        }
+    }    
+}
