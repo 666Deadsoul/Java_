cc25e08aa6316e887a5890b8dc420b427ac96ef0
666Deadsoul <sarinamaharjan234@gmail.com>
2024-06-11 21:59:15 +0545

    find socket

---
+++ new/src/InetAddress/SocketCheck.java
@@ -0,0 +1,19 @@
+package InetAddress;
+import java.net.*;
+
+//to check the socket
+public class SocketCheck {
+    public static void main(String[] args) {
+        try{
+            NetworkInterface n = NetworkInterface.getByName("eth0");
+            if(n == null){
+                System.out.println("No such interface");
+            }else{
+                System.out.println(n);
+            }
+        }
+        catch(SocketException ex){
+            System.out.println(ex);
+        }
+    }
+}
