ec0f821f446209d8f1a9336b7b67cbb5412e50a3
666Deadsoul <sarinamaharjan234@gmail.com>
2024-06-17 14:55:02 +0545

    get element using index

---
+++ new/src/JavaCollection/Nmaes.java
@@ -0,0 +1,22 @@
+package JavaCollection;
+    import java.util.ArrayList;
+    import java.util.Scanner;
+    
+  //to get element using index  
+class Names {
+        public static void main(String[] args) {
+            ArrayList<String> names = new ArrayList<>();
+            names.add("John");
+            names.add("Harry");
+            names.add("Niall");
+    
+            Scanner input = new Scanner(System.in);
+            int index = input.nextInt();
+
+            String ele = names.get(index);
+            System.out.println(ele);
+    
+            input.close();
+        }
+    }
+
