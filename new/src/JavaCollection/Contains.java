f62ded15ba762fb7da46ddf9cff17ab9c108e91c
666Deadsoul <sarinamaharjan234@gmail.com>
2024-06-17 15:07:41 +0545

    using contains keyword to check if it is in arraylist or not

---
+++ new/src/JavaCollection/Contains.java
@@ -0,0 +1,22 @@
+package JavaCollection;
+import java.util.ArrayList;
+
+//to check if the value is in array list or not
+
+class Contains {
+    public static void main(String[] args) {
+        ArrayList<String> courses = new ArrayList<>();
+        courses.add("Java");
+        courses.add("C++");
+        courses.add("DSA");
+
+        System.out.println("Is DSA present in courses arraylist: ");
+        if (courses.contains("DSA")) {
+            System.out.println("DSA is available");
+        }
+
+        else {
+            System.out.println("DSA is not available");
+        }
+    }
+}
