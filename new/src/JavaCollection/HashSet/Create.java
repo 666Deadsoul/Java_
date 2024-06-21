e357bae71fab06e890d8d533d088e878caa94645
666Deadsoul <sarinamaharjan234@gmail.com>
2024-06-21 18:57:05 +0545

    Hashset Creation

---
+++ new/src/JavaCollection/HashSet/Create.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+class Main {
+    public static void main(String[] args) {
+
+        HashSet<String> animals = new HashSet<>();
+
+        animals.add("tiger");
+        animals.add("dog");
+        animals.add("elephant");
+
+        System.out.println(animals);
+    }
+}
