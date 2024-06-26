3d65537ee1b412751ce20268ea983d87b4fa7fbf
666Deadsoul <sarinamaharjan234@gmail.com>
2024-06-26 19:23:22 +0545

    Directory creation

---
+++ new/src/FileHandling/DirCreate.java
@@ -0,0 +1,19 @@
+package FileHandling;
+import java.io.File;
+
+class Main {
+    public static void main(String[] args) {
+
+        // create object of File
+        File file = new File("movies");
+
+        // try to create a new directory
+        boolean value = file.mkdir();
+        if (value) {
+            System.out.println("The new directory is created.");
+        } 
+        else {
+            System.out.println("The directory already exists.");
+        }
+    }
+}
