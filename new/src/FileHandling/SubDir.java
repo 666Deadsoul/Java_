cc0f8651e84284619c1b549ded1e4466d6b60071
666Deadsoul <sarinamaharjan234@gmail.com>
2024-06-26 19:24:40 +0545

    Sub directory creation

---
+++ new/src/FileHandling/SubDir.java
@@ -0,0 +1,20 @@
+package FileHandling;
+
+import java.io.File;
+
+class Main {
+    public static void main(String[] args) {
+
+        // creates a file object with the specified path
+        File file = new File("movies/John Wick");
+
+        // tries to create a new directory
+        boolean value = file.mkdir();
+        if (value) {
+            System.out.println("The subdirectory is created.");
+        } 
+        else {
+            System.out.println("The subdirectory already exists.");
+        }
+    }
+}
